3f10c2c6d7374300c12715e9e243f279a402ed90
guenneguezt <thomas@guenneguez.com>
2017-10-02 22:33:25 +0200

    Suppression lien paypal

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -68,14 +68,5 @@ if (!isConnect('admin')) {
 			Permet d'avoir plus de message dans la log du daemon.
         </div>
     </div>
-	<div class="form-group">
-		<label class="col-lg-4 control-label">{{Faire un don au développeur}}</label>
-		<div class="col-lg-5">
-			Ce plugin est gratuit pour que chacun puisse en profiter simplement. Si vous souhaitez tout de même faire un don au développeur du plugin, utilisez le lien suivant.<br>
-			<a class="btn" id="bt_paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C7MF4WU7SWNKG" target="_new" >
-				<img src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" alt="{{Faire un don via Paypal au développeur}}">
-			</a>
-		</div>
-	</div>
 </form>
 <?php include_file('desktop', 'ftpd', 'js', 'ftpd'); ?>
