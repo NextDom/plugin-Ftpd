e35c69e8f34e1aabc1e399787dfd3110205f8c88
guenneguezt <thomas@guenneguez.com>
2018-03-28 00:12:01 +0200

    Tester demon python

---
+++ test/testFtpd.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd ${TRAVIS_BUILD_DIR}/ressources
+python ./ftpd.py test
+if [ $? != 0 ]
+then
+  echo Unable to test daemon
+  exit 1
+fi
+cat ftpd_config_sample.xml | sed "s?/var/www/html/plugins/ftpd/core/class/../../../../log/ftpd_daemon?${TRAVIS_BUILD_DIR}/tmp/ftpd_daemon?" | sed "s?/var/www/html/plugins/ftpd/core/class/../../capture/?${TRAVIS_BUILD_DIR}/tmp/capture/?" | sed "s?/var/www/html/plugins/ftpd/core/class/../../ressources/ftpd.pid?${TRAVIS_BUILD_DIR}/tmp/ftpd.pidn?" > ftpd.xml
+cat ftpd.xml
+mkdir ../tmp
+mkdir ../tmp/capture
+python ./ftpd.py start
+if [ $? != 0 ]
+then
+  echo Unable to start daemon
+  exit 1
+fi
+sleep 60
+python ./ftp.py
+if [ $? != 0 ]
+then
+  exit 1
+  echo Unable to start client
+fi
+python ./ftpd.py stop
+if [ $? != 0 ]
+then
+  echo Unable to stop daemon
+  exit 1
+fi
+cat ../tmp/ftpd_daemon
+ls -lR ../tmp/
