e0ae7a307f0088357c87d61d30ac3bb929f18cce
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-03-28 21:29:14 +0200

    remove ?> and displayException

--- core/ajax/ftpd.ajax.php
+++ core/ajax/ftpd.ajax.php
@@ -47,6 +47,5 @@ try {
     throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
     /*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
+    ajax::error(displayException($e), $e->getCode());
 }
-?>
