0b1a47d5174651c31e2ee5368036861485cff2bd
guenneguezt <thomas@guenneguez.com>
2018-06-18 23:34:09 +0200

    Correction FtpdConstants

--- core/class/ftpdConstants.class.php
+++ core/class/ftpdConstants.class.php
@@ -5,7 +5,7 @@ class FtpdConstants
   const FIELD_FILTRE = 'filtre';
   const DEFAULT_IMAGE = '../img/no-image.png';
   const DEFAULT_PORT = '8888';
-  const DEFAULT_LOCAL_IP = 0.0.0.0;
+  const DEFAULT_LOCAL_IP = '0.0.0.0';
 
   const DEBUG_FACILITY = 'debug';
   const INFO_FACILITY = 'info';
